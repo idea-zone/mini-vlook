4bc559b42cc4d247cd8b48c75a0a50b5f1a02944
bug320 <1046218884@qq.com>
2022-04-22 23:31:44 +0800

    添加刮刮乐官方样式

---
+++ script/module/code-rb-coad.js
@@ -0,0 +1,12 @@
+
+/**
+ * 目标：刮刮卡
+ */
+
+
+// 未刮开
+// <code class="v-rb-coat" data-v-rb-coat-data="这是刮刮卡的内容" data-v-rb-coat-showed="false" title="点击查看有效的原始内容" style="background: linear-gradient(45deg, var(--ac-gray-lg) 0%, var(--ac-gray-lg) 25%,var(--d-f-c) 25%, var(--d-f-c) 50%,var(--ac-gray-lg) 50%, var(--ac-gray-lg) 75%,var(--d-f-c) 75%, var(--d-f-c) 100%); border-color: var(--ac-gray-lg); color: var(--d-bc);"> 提示信息 </code>
+
+// 刮开
+// <code class="v-rb-coat opened" data-v-rb-coat-data=" 提示信息 " data-v-rb-coat-showed="true" title="点击查看有效的原始内容" style="background: linear-gradient(45deg, var(--ac-gray-lg) 0%, var(--ac-gray-lg) 25%,var(--d-f-c) 25%, var(--d-f-c) 50%,var(--ac-gray-lg) 50%, var(--ac-gray-lg) 75%,var(--d-f-c) 75%, var(--d-f-c) 100%); border-color: var(--ac-gray-lg); color: rgb(148, 152, 160);">这是刮刮卡的内容</code>
+
