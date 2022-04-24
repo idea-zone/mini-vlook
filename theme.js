6585edb5d03b07ecaabe8bd088db6b29e354279b
bug320 <1046218884@qq.com>
2022-04-25 01:31:39 +0800

    添加计数任务

--- theme.js
+++ theme.js
@@ -162,6 +162,7 @@ window.theme.changeThemeMode(
 window.theme.loadScript("/appearance/themes/mini-vlook/script/module/codelabel.js");
 window.theme.loadScript("/appearance/themes/mini-vlook/script/module/code-rb-coad.js");
 window.theme.loadScript("/appearance/themes/mini-vlook/script/module/codelabel-pg.js");
+window.theme.loadScript("/appearance/themes/mini-vlook/script/module/codelabel-todo.js");
 
 // window.theme.loadScript("/appearance/themes/Dark+/script/module/background.js");
 // window.theme.loadScript("/appearance/themes/Dark+/script/module/blockattrs.js");
