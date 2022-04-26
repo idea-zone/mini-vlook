84a717b83343b1495cfe4530f5da8ed1aafdaa86
bug320 <1046218884@qq.com>
2022-04-26 09:58:42 +0800

    清理项目

--- script/module/b320config.js
+++ script/module/b320config.js
@@ -9,14 +9,7 @@ export var config = {
             time: /^(\d+)(:[0-5]?[0-9]){0,2}(\.\d*)?$/, // 时间戳正则表达式
             id: /^\d{14}\-[0-9a-z]{7}$/, // 块 ID 正则表达式
             
-            // 正则表达式
-            // wz:'#(.*)?[|](.*)?#([\(](#?[\\d\\w]+)(!)?[\)])?',
-            wz: '(#(.*?)[|](.*?)#){1,1}?([\(](#?[\\d\\w]+)(!)?[\)])?',   // 非贪婪匹配#号，允许用#开头的颜色
             colorvalue: '^#([0-9a-fA-F]{6}|[0-9a-fA-F]{3})$',            // 匹配#开头的颜色
-            // `*{ 提示信息 }(这是刮刮卡的内容)`
-            rb:'^\\\*\\\{(.*)\\\}\\\((.*?)(\\s*\\\"(#?[\\d\\w]+)\\\")?\\\)$',           // 匹配刮刮卡内容
-            pg:'^\\\{(.*)\\\}\\s*\\\((.*)\\\)$',           // 匹配注音内容
-            todo:'^\\\+\\\[(.*)\\\]\\\((.*)\\\)$',           // 带计数功能的任务项
         },
 
         common: {
@@ -153,13 +146,6 @@ export var config = {
                         'msgcolor': '2b1c29',
                     }
                 },
-                default: "theme2",      // 默认的颜色值
-                wz: {
-                    bgcolor: '#2aa899',    // 微章背景色
-                    title_color: '#eaf6f5', // 微章标题字体色
-                    msg_color: '#1f2e3b',   // 微章内容字体色
-                    msg_bgcolor: 'rgba(255,255,255,0.6)', // 微章内容背景色
-                }
             },
         },
         codelabel: {
@@ -182,80 +168,62 @@ export var config = {
                 },
             },
             ptype: [       // 解析类型
-                {   // 计数任务
-                    typeid: "todo",
-                    reg: '\\\+\\\[(\\d+)\\\]\\s*\\\((.*)\\\)',  // 正则表达式
-                    customf: 'todo',                        // 忽略解析的属性值 
-                    className: 'vk-todo',                   // 自定义的属性名称
+                {   // 微章
+                    typeid: "wz",
+                    reg: '(#(.*?)[|](.*?)#){1,1}?([\(](#?[\\d\\w]+)(!)?[\)])?',  // 正则表达式
+                    customf: 'wz',                        // 忽略解析的属性值 
+                    className: 'custom-codelabel-wz',                   // 自定义的属性名称
                     maps: { // 解析后-分组的别名，也是 parseInfo 中的字段
+                        /**
+                         * 以下字段名称被占用,不要用于下面列表的值中.
+                         * value,             // code 标签的 InnerHTML
+                         * color1,bgcolor1,   // 主颜色计算结果和适配背景色
+                         * color2,bgcolor2,   // 次颜色计算结果和适配背景色
+                         * $0~$9 也不要用.  
+                         */
                         '$0': 'value', // 占用，code 原始的 innerHTML 内容
-                        '$1': 'count',
-                        '$2': 'data',
-                        '$3': '',
+                        '$1': '',
+                        '$2': 'title',
+                        '$3': 'msg',
                         '$4': '',
-                        '$5': '',
-                        '$6': '',
+                        '$5': 'color',
+                        '$6': 'endsuffix',
                         '$7': '',
                         '$8': '',
                         '$9': '',
                     },
 
-                    emptys: ['count', 'data'],      // 不能为空的字段
+                    emptys: ['title','msg'],      // 不能为空的字段
                     emptysValues:{              // 当值为空值的值
                     },
                     style:{ // 样式映射信息
-                        rerender:false,             // 是否计算配色
-                        // color: {
-                        //     value:'color',            // 主颜色字段
-                        //     suffix:'',                // 颜色后缀对应的字段
-                        // },         
-                        // default:'gray',               // 缺省颜色值
-                        // defaultSuffix:false, // 缺省时,颜色后缀,对应的值.
-                        // colors:{
-                        //     suffixs:{
-                        //         '!':true,
-                        //     },
-                        //     names: ()=>config.theme.common.colors.names,   // 颜色名称-列表
-                        //     values: ()=>config.theme.common.colors.values, // 适配配色-列表
-                        // }
+                        rerender:true,             // 是否计算配色
+                        color: {
+                            value:'color',            // 主颜色字段
+                            suffix:'endsuffix',                // 颜色后缀对应的字段
+                        },         
+                        default:'theme2',               // 缺省颜色值
+                        defaultSuffix:false, // 缺省时,颜色后缀,对应的值.
+                        colors:{
+                            suffixs:{
+                                '!':true,
+                            },
+                            names: ()=>config.theme.common.colors.names,   // 颜色名称-列表
+                            values: ()=>config.theme.common.colors.values, // 适配配色-列表
+                        }
                     },
                     customAttr: { // 自定义属性
-                        'custom-codelabel-todo-count': "${count}",
-                        'custom-codelabel-todo-data': "${data}",
+                        'custom-codelabel-wz-title': "${title}",
+                        'custom-codelabel-wz-msg': "${msg}",
                     },
                     inlineStyle: {
+                        "--theme-wz-bgcolor":"${bgcolor1}",
+                        "--theme-wz-title-color":"${color1}",
+                        "--theme-wz-msg-color":"${color2}",
+                        "--theme-wz-msg-bgcolor":"${bgcolor2}",
                     },
-                    innerHTML: '<button>+</button><span>[${count}]</span><span>(</span>${data}<span>)</span>',
-                    renderEnd: (parse, element,oldHTML) => {
-
-                        function bingOnClick(parse, e,oldHTML) {
-
-                            parse.reinitFormat(parse.ptypeItem)
-                            
-                            let parseInfo = parse.clacParseInfo(oldHTML);
-
-                            let c = 1 + +parseInfo.count;
-                            parseInfo.count = ""+c;
-
-                            parse.parseInfo = parseInfo;
-                            parse.renderSingle(e, parseInfo);
-
-                            // 这里已经更新了，所有旧的 oldHTML 和 parse.Value 就没用了。重新组合
-                            e.firstChild.onclick = bingOnClick.bind(e.firstChild, parse, e,`+[${parse.parseInfo.count}](${parse.parseInfo.data})`)
-
-                            e.firstChild.setAttribute(
-                                "custom-codelabel-todo-count",
-                                parse.parseInfo.count
-                            );
-
-                        }
-
-                        element.firstChild.onclick = bingOnClick.bind(element.firstChild, parse, element,oldHTML)
-                        element.firstChild.setAttribute(
-                            "custom-codelabel-todo-count",
-                            element.getAttribute("custom-codelabel-todo-count"),
-                        );
-
+                    innerHTML:  '<span>${value}</span>',
+                    renderEnd: (parse, element,oldHTML) => { // 渲染完单个元素的回调.
                     },
                 },
                 {   // 刮刮乐
@@ -379,62 +347,80 @@ export var config = {
                     renderEnd: (parse, element,oldHTML) => { // 渲染完单个元素的回调.
                     },
                 },
-                {   // 微章
-                    typeid: "wz",
-                    reg: '(#(.*?)[|](.*?)#){1,1}?([\(](#?[\\d\\w]+)(!)?[\)])?',  // 正则表达式
-                    customf: 'wz',                        // 忽略解析的属性值 
-                    className: 'custom-codelabel-wz',                   // 自定义的属性名称
+                {   // 计数任务
+                    typeid: "todo",
+                    reg: '\\\+\\\[(\\d+)\\\]\\s*\\\((.*)\\\)',  // 正则表达式
+                    customf: 'todo',                        // 忽略解析的属性值 
+                    className: 'vk-todo',                   // 自定义的属性名称
                     maps: { // 解析后-分组的别名，也是 parseInfo 中的字段
-                        /**
-                         * 以下字段名称被占用,不要用于下面列表的值中.
-                         * value,             // code 标签的 InnerHTML
-                         * color1,bgcolor1,   // 主颜色计算结果和适配背景色
-                         * color2,bgcolor2,   // 次颜色计算结果和适配背景色
-                         * $0~$9 也不要用.  
-                         */
                         '$0': 'value', // 占用，code 原始的 innerHTML 内容
-                        '$1': '',
-                        '$2': 'title',
-                        '$3': 'msg',
+                        '$1': 'count',
+                        '$2': 'data',
+                        '$3': '',
                         '$4': '',
-                        '$5': 'color',
-                        '$6': 'endsuffix',
+                        '$5': '',
+                        '$6': '',
                         '$7': '',
                         '$8': '',
                         '$9': '',
                     },
 
-                    emptys: ['title','msg'],      // 不能为空的字段
+                    emptys: ['count', 'data'],      // 不能为空的字段
                     emptysValues:{              // 当值为空值的值
                     },
                     style:{ // 样式映射信息
-                        rerender:true,             // 是否计算配色
-                        color: {
-                            value:'color',            // 主颜色字段
-                            suffix:'endsuffix',                // 颜色后缀对应的字段
-                        },         
-                        default:'theme2',               // 缺省颜色值
-                        defaultSuffix:false, // 缺省时,颜色后缀,对应的值.
-                        colors:{
-                            suffixs:{
-                                '!':true,
-                            },
-                            names: ()=>config.theme.common.colors.names,   // 颜色名称-列表
-                            values: ()=>config.theme.common.colors.values, // 适配配色-列表
-                        }
+                        rerender:false,             // 是否计算配色
+                        // color: {
+                        //     value:'color',            // 主颜色字段
+                        //     suffix:'',                // 颜色后缀对应的字段
+                        // },         
+                        // default:'gray',               // 缺省颜色值
+                        // defaultSuffix:false, // 缺省时,颜色后缀,对应的值.
+                        // colors:{
+                        //     suffixs:{
+                        //         '!':true,
+                        //     },
+                        //     names: ()=>config.theme.common.colors.names,   // 颜色名称-列表
+                        //     values: ()=>config.theme.common.colors.values, // 适配配色-列表
+                        // }
                     },
                     customAttr: { // 自定义属性
-                        'custom-codelabel-wz-title': "${title}",
-                        'custom-codelabel-wz-msg': "${msg}",
+                        'custom-codelabel-todo-count': "${count}",
+                        'custom-codelabel-todo-data': "${data}",
                     },
                     inlineStyle: {
-                        "--theme-wz-bgcolor":"${bgcolor1}",
-                        "--theme-wz-title-color":"${color1}",
-                        "--theme-wz-msg-color":"${color2}",
-                        "--theme-wz-msg-bgcolor":"${bgcolor2}",
                     },
-                    innerHTML:  '<span>${value}</span>',
-                    renderEnd: (parse, element,oldHTML) => { // 渲染完单个元素的回调.
+                    innerHTML: '<button>+</button><span>[${count}]</span><span>(</span>${data}<span>)</span>',
+                    renderEnd: (parse, element,oldHTML) => {
+
+                        function bingOnClick(parse, e,oldHTML) {
+
+                            parse.reinitFormat(parse.ptypeItem)
+                            
+                            let parseInfo = parse.clacParseInfo(oldHTML);
+
+                            let c = 1 + +parseInfo.count;
+                            parseInfo.count = ""+c;
+
+                            parse.parseInfo = parseInfo;
+                            parse.renderSingle(e, parseInfo);
+
+                            // 这里已经更新了，所有旧的 oldHTML 和 parse.Value 就没用了。重新组合
+                            e.firstChild.onclick = bingOnClick.bind(e.firstChild, parse, e,`+[${parse.parseInfo.count}](${parse.parseInfo.data})`)
+
+                            e.firstChild.setAttribute(
+                                "custom-codelabel-todo-count",
+                                parse.parseInfo.count
+                            );
+
+                        }
+
+                        element.firstChild.onclick = bingOnClick.bind(element.firstChild, parse, element,oldHTML)
+                        element.firstChild.setAttribute(
+                            "custom-codelabel-todo-count",
+                            element.getAttribute("custom-codelabel-todo-count"),
+                        );
+
                     },
                 },
             ],
