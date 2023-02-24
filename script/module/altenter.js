cb90df47e5b15160730076aad33a5ebc47521f85
bug320 <1046218884@qq.com>
2023-02-24 17:14:32 +0800

    解决自定义的 ALT+Enter 导致的光标定位错乱问题

--- script/module/altenter.js
+++ script/module/altenter.js
@@ -4,12 +4,17 @@ import { isKey } from '../utils/hotkey.js';
 
 import { mv } from '../commons/domex.js';
 
+var lastEditRange;
+
 (() => {
     try {
 
         let body = document.body;
         
         body.addEventListener('keyup', (e) => {
+
+            let selection = getSelection();
+            lastEditRange = selection.getRangeAt(0);
 
             var el=mv.GetSiyuanBlock(document.getSelection().focusNode.parentElement);
 
@@ -34,20 +39,43 @@ import { mv } from '../commons/domex.js';
                     console.log("AltEnter");
                   
                     if (nel.innerHTML.match('\n\n')){
-                        let selection = getSelection();
-
-                        // {: style="color: red;--before-ctx:'♯ '"}
-                        nel.innerHTML = `${nel.innerHTML.replace('\n\n','\n')}<span data-type="em" style="color: gray;--before-ctx:'♯ '"> </span>`
                         
-                        var range = selection.getRangeAt(0)
+                        let selection = getSelection();
+                        nel.innerHTML = `${nel.innerHTML.replace('\n\n','\n')}<span data-type="em" style="color: gray;--before-ctx:'♯ '"> </span>`
+                        el.focus();
+                        selection = getSelection();
+                        if (lastEditRange) {
+                            // 存在最后光标对象，选定对象清除所有光标并添加最后光标还原之前的状态
+                            selection.removeAllRanges()
+                            selection.addRange(lastEditRange)
+                        }
+                        // var range = selection.getRangeAt(selection.rangeCount-1);
+                        // var textNode = range.endContainer;
+                        var range = selection.getRangeAt(0);
                         var textNode = range.startContainer;
                         // 处理光标位置
                         var rangeStartOffset = range.startOffset;
-                        range.setStart(textNode, rangeStartOffset + 2)
-                        range.collapse(true)
 
+                        // 获取最后一个 span 并focs ，解决关闭定位错乱问题
+                        var nodes=textNode.querySelectorAll('span')
+                        var first = nodes[0];
+                        var as = nodes[nodes.length- 1];
+                        as.focus();
+                        if (range) {
+                            // 存在最后光标对象，选定对象清除所有光标并添加最后光标还原之前的状态
+                            selection.removeAllRanges()
+                            selection.addRange(range)
+                        }
+                        range = selection.getRangeAt(0);
+                        textNode = range.startContainer;
+                        var rangeStartOffset = range.startOffset;
+                        range.setStart(as, rangeStartOffset+1);
+
+                        range.collapse(true);
                         selection.removeAllRanges()
                         selection.addRange(range)
+                        // as.focus();
+                        lastEditRange = selection.getRangeAt(0);
                     }
 
                     //#region 旧版
@@ -71,6 +99,7 @@ import { mv } from '../commons/domex.js';
                     //#endregion
                 }
             }
+
             
         })
 
